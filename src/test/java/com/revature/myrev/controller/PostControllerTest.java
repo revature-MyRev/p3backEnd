5699f8aeef595643bd2acc33866ebd7205bf58b5
jessphorn <jessphorn@gmail.com>
2021-11-17 15:54:01 -0500

    Added junit test classes to test packages

---
+++ src/test/java/com/revature/myrev/controller/PostControllerTest.java
@@ -0,0 +1,14 @@
+package com.revature.myrev.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PostControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/revature/myrev/controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+package com.revature.myrev.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
