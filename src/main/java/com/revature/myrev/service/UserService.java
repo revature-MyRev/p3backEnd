5c8ecd37d9abee4a9162d15cfe5e014a57e031c1
Damian0gre <91745291+Damian0gre@users.noreply.github.com>
2021-11-18 14:43:56 -0500

    Update UserService.java

--- src/main/java/com/revature/myrev/service/UserService.java
+++ src/main/java/com/revature/myrev/service/UserService.java
@@ -1,20 +1,30 @@
 package com.revature.myrev.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.revature.myrev.model.User;
+import com.revature.myrev.repository.UserRepository;
 
-public interface UserService {
+@Service
+public class UserService {
 	
-    public User findByUsername(String username);
+	@Autowired
+	UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(int userId) {
+		return repository.findById(userId).get();
+	}
+
+	public void save(User user) {
+		repository.save(user);
+	}
 	
-	public List<User> findAll();
-
-	public User findById(int id);
-
-	public void save(User user);
-
-	public void update(int id, User user);
-
-	public void deleteById(int id);
 }
