eede90d369154818ad2b34a753c8c6ba2de817e4
Cole A. Kendall <kendall.cole@revature.com>
2021-11-18 02:04:53 -0500

    add implementation files for Thread Service

--- src/main/java/com/revature/myrev/service/ThreadService.java
+++ src/main/java/com/revature/myrev/service/ThreadService.java
@@ -1,13 +1,22 @@
 package com.revature.myrev.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.myrev.model.Follower;
 import com.revature.myrev.repository.ThreadRepository;
 
-@Service
-public class ThreadService {
+public interface ThreadService {
+	
+	public List<Thread> findAll();
 
-	@Autowired
-	ThreadRepository repository;
+	public Thread findById(int id);
+
+	public void save(Thread thread);
+
+	public void update(int id, Thread thread);
+
+	public void deleteById(int id);
 }
