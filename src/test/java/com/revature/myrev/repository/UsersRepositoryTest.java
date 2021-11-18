10bf4247f1bdb86d605a2c5f312d1d683f7478c3
StevenSalmeron <92335046+StevenSalmeron@users.noreply.github.com>
2021-11-18 12:12:34 -0800

    adding tests for user repository

--- src/test/java/com/revature/myrev/repository/UsersRepositoryTest.java
+++ src/test/java/com/revature/myrev/repository/UsersRepositoryTest.java
@@ -1,37 +1,54 @@
 package com.revature.myrev.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.myrev.MyRevApplication;
+import com.revature.myrev.model.Users;
 
 @SpringBootTest
 @ContextConfiguration(classes = MyRevApplication.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Rollback(false)
-@DataJpaTest
+@Transactional
+
 class UsersRepositoryTest {
-	
-	// Not mocking anything here. 
+
+	// Not mocking anything here.
 	// Be sure to delete any additions made to table at the end of each test
 	/** UserRepository for testing */
 	@Autowired
 	private UsersRepository repository;
 
 	@Test
-	public void testFindByUsername () {
-		//test for exception on empty table
-		
-		//test for exception on invalid username
-		
-		//test for returned user on valid username
+	@Order(1)
+	public void testCreateUsers() {
+
+		Users testUser = new Users(0, 20, "Tester1234", "test123", "Female", "null", "test@revature.com", "Test",
+				"Testing", "JUnit", "Tester");
+
+		Users result = repository.save(testUser);
+
+		Assertions.assertNotEquals(0, result.getUserId());
 	}
+
+	@Test
+	@Order(2)
+	public void testFindByUsername() {
+		Users testUser = new Users(0, 20, "Tester1234", "test123", "Female", "null", "test@revature.com", "Test",
+				"Testing", "JUnit", "Tester");
+
+		repository.save(testUser);
+
+		Users result = repository.findByUserName(testUser.getUserName());
+
+		Assertions.assertEquals(testUser.getAge(), result.getAge());
+	}
+
 }
