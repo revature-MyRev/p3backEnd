d2381d4f41bb4247831b3bcf3418caacc2bb958d
airmanon <anjali.ghosh@revature.net>
2021-11-18 12:32:08 -0800

    Create package-info.java

---
+++ src/main/java/com/revature/myrev/config/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package will be used to hold files that are needed to help incorporate tokens into the project
+ */
+/**
+ * @author Dipanjali Ghosh
+ *
+ */
+package com.revature.myrev.config;
