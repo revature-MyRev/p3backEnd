3c43295e26f1439c392ea03a96a57abc9b02e48b
airmanon <anjali.ghosh@revature.net>
2021-11-18 12:04:01 -0800

    Create TokenDemoController.java

---
+++ src/main/java/com/revature/myrev/controller/TokenDemoController.java
@@ -0,0 +1,13 @@
+/**
+ * 	This Controller file has been added to practice 
+ * 		using JWT tokens without messing with the rest of the project.
+ */
+package com.revature.myrev.controller;
+
+/**
+ * @author Dipanjali Ghosh
+ *
+ */
+public class TokenDemoController {
+
+}
