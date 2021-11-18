c55d688f2becd194f5fc493cec9cddeab06ff9da
airmanon <anjali.ghosh@revature.net>
2021-11-18 09:01:44 -0800

    Update UsersTest.java
    Added all Unit Tests for UsersTest.java. All tests pass.

--- src/test/java/com/revature/myrev/model/UsersTest.java
+++ src/test/java/com/revature/myrev/model/UsersTest.java
@@ -18,116 +18,187 @@ class UsersTest {
 
 	@Test
 	void userIDGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertEquals(0, testUser.getUserId());
 	}
 	
 	@Test
 	void userIDSetterTest() {
-		fail("Not yet implemented");
+		int test = 1;
+		Users testUser = new Users();
+		testUser.setUserId(test);
+		assertEquals(test, testUser.getUserId());
 	}
 	
 	@Test
 	void ageGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertEquals(0, testUser.getAge());
 	}
 	
 	@Test
 	void ageSetterTest() {
-		fail("Not yet implemented");
+		int test = 26;
+		Users testUser = new Users();
+		testUser.setAge(test);
+		assertEquals(test, testUser.getAge());
 	}
 	
 	@Test
 	void userNameGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertNull(testUser.getUserName(), "testUser.username not null");
 	}
 	
 	@Test
 	void userNameSetterTest() {
-		fail("Not yet implemented");
+		String test = "testUserName";
+		Users testUser = new Users();
+		testUser.setUserName(test);
+		assertEquals(0, testUser.getUserName().compareTo(test));
 	}
 	
 	@Test
 	void passwordGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertNull(testUser.getPassword(), "testUser.password not null");
 	}
 	
 	@Test
 	void passwordSetterTest() {
-		fail("Not yet implemented");
+		String test = "testPassword";
+		Users testUser = new Users();
+		testUser.setPassword(test);
+		assertEquals(0, testUser.getPassword().compareTo(test));
 	}
 	
 	@Test
 	void genderGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertNull(testUser.getGender(), "testUser.gender not null");
 	}
 	
 	@Test
 	void genderSetterTest() {
-		fail("Not yet implemented");
+		String test = "Non-Binary";
+		Users testUser = new Users();
+		testUser.setGender(test);
+		assertEquals(0, testUser.getGender().compareTo(test));
 	}
 	
 	@Test
 	void photoGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertNull(testUser.getPhoto(), "testUser.photo not null");
 	}
 	
 	@Test
 	void photoSetterTest() {
-		fail("Not yet implemented");
+		String test = "testPhoto.jpg";
+		Users testUser = new Users();
+		testUser.setPhoto(test);
+		assertEquals(0, testUser.getPhoto().compareTo(test));
 	}
 	
 	@Test
 	void emailGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertNull(testUser.getEmail(), "testUser.email not null");
 	}
 	
 	@Test
 	void emailSetterTest() {
-		fail("Not yet implemented");
+		String test = "first.last@revature.net";
+		Users testUser = new Users();
+		testUser.setEmail(test);
+		assertEquals(0, testUser.getEmail().compareTo(test));
 	}
 	
 	@Test
 	void firstNameGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertNull(testUser.getFirstName(), "testUser.firstName not null");
 	}
 	
 	@Test
 	void firstNameSetterTest() {
-		fail("Not yet implemented");
+		String test = "First";
+		Users testUser = new Users();
+		testUser.setFirstName(test);
+		assertEquals(0, testUser.getFirstName().compareTo(test));
 	}
 	
 	@Test
 	void lastNameGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertNull(testUser.getLastName(), "testUser.lastName not null");
 	}
 	
 	@Test
 	void lastNameSetterTest() {
-		fail("Not yet implemented");
+		String test = "Last";
+		Users testUser = new Users();
+		testUser.setLastName(test);
+		assertEquals(0, testUser.getLastName().compareTo(test));
 	}
 	
 	@Test
 	void middleNameGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertNull(testUser.getMiddleName(), "testUser.middleName not null");
 	}
 	
 	@Test
 	void middleNameSetterTest() {
-		fail("Not yet implemented");
+		String test = "Middle";
+		Users testUser = new Users();
+		testUser.setMiddleName(test);
+		assertEquals(0, testUser.getMiddleName().compareTo(test));
 	}
 	
 	@Test
 	void jobTitleGetterTest() {
-		fail("Not yet implemented");
+		Users testUser = new Users();
+		assertNull(testUser.getJobTitle(), "testUser.jobTitle not null");
 	}
 	
 	@Test
 	void jobTitleSetterTest() {
-		fail("Not yet implemented");
+		String test = "testJobTitle";
+		Users testUser = new Users();
+		testUser.setJobTitle(test);
+		assertEquals(0, testUser.getJobTitle().compareTo(test));
 	}
 	
 	@Test
 	void usersToStringTest() {
-		fail("Not yet implemented");
+		String test = "User [userId=" + "0" + ", age=" + "0" + ", userName=" + "null" + ", gender=" + "null" + ", photo="
+				+ "null" + ", email=" + "null" + ", firstName=" + "null" + ", lastName=" + "null" + ", middleName="
+				+ "null" + ", jobTitle=" + "null" + "]";
+		Users testUser = new Users();
+//		System.out.println("test = " + test);	// These two print statements were added to figure out just what happens with toString
+//		System.out.println("testUser.toString() = " + testUser);
+		assertEquals(0, testUser.toString().compareTo(test));
+	}
+	
+	@Test
+	void usersConstructorWithParamsTest() {
+		int userId = 1;
+		int age = 26;
+		String userName = "testUserName";
+		String password = "testPassword";
+		String gender = "Non-binary";
+		String photo = "testPhoto.jpg";
+		String email = "first.last@revature.net";
+		String firstName = "First";
+		String lastName = "Last";
+		String middleName = "Middle";
+		String jobTitle = "testJobTitle";
+		String test = "User [userId=" + userId + ", age=" + age + ", userName=" + userName + ", gender=" + gender + ", photo="
+				+ photo + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName + ", middleName="
+				+ middleName + ", jobTitle=" + jobTitle + "]";
+		Users testUser = new Users(userId, age, userName, password, gender, photo, email,
+				firstName, lastName, middleName, jobTitle);
+		assertEquals(0, testUser.toString().compareTo(test));
 	}
 }
