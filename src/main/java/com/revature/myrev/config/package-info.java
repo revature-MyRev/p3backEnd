d9a6bb4c1fd2bae731aa9072cba0445063f7348f
Jessica <33468876+jessphorn@users.noreply.github.com>
2021-11-19 11:21:21 -0500

    Delete src/main/java/com/revature/myrev/config directory

--- src/main/java/com/revature/myrev/config/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package will be used to hold files that are needed to help incorporate tokens into the project
- */
-/**
- * @author Dipanjali Ghosh
- *
- */
-package com.revature.myrev.config;
