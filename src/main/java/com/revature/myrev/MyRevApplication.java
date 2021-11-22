20d8734e83dced755bfa022ad2ff8670749f3d92
cscarnes <61205352+cscarnes@users.noreply.github.com>
2021-11-22 13:49:53 -0500

    Updated post class

--- src/main/java/com/revature/myrev/MyRevApplication.java
+++ src/main/java/com/revature/myrev/MyRevApplication.java
@@ -3,11 +3,15 @@ package com.revature.myrev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.revature.myrev.model.Post;
+
 @SpringBootApplication
 public class MyRevApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRevApplication.class, args);
+		
+		Post post = new Post();
 	}
 
 }
