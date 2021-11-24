19147f773029acd8385ac80ce36fad821ab43126
airmanon <anjali.ghosh@revature.net>
2021-11-24 12:47:44 -0800

    Update RoleRepositoryTest.java
    Note: Had to change application.properties locally to put the database on postgres as the application context failed to load when I tried to do it with MySQL

--- src/test/java/com/revature/myrev/repository/RoleRepositoryTest.java
+++ src/test/java/com/revature/myrev/repository/RoleRepositoryTest.java
@@ -1,26 +1,19 @@
 package com.revature.myrev.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
+//import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
+//import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.myrev.MyRevApplication;
 import com.revature.myrev.model.ERole;
-import com.revature.myrev.model.Role;
 import com.revature.myrev.model.Role;
 
 /**
@@ -29,57 +22,57 @@ import com.revature.myrev.model.Role;
  */
 @SpringBootTest
 @ContextConfiguration(classes = MyRevApplication.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Rollback(false)
 @Transactional
-public class RoleRepositoryTest {
+class RoleRepositoryTest {
 	// Not mocking anything here.
 	// Be sure to delete any additions made to table at the end of each test
 	/** RoleRepository for testing */
 	@Autowired
 	private RoleRepository repository;
-	
+
 	/**
-	 * Tests for a successful addition to the Role table 
+	 * Tests for a successful addition to the Role table
 	 */
 	@Test
 	@Order(1)
-	public void testCreateRoles() {
+	void testCreateRoles() {
 
 		Role testRole = new Role(1, ERole.ROLE_USER);
 
 		Role result = repository.save(testRole);
 
 		Assertions.assertNotEquals(0, result.getRoleId());
-		Assertions.assertEquals(testRole.toString(), result.toString());
+		Assertions.assertEquals(testRole.getRoleId(), result.getRoleId());
 	}
 
 	/**
-	 * Tests findByUserName
-	 * Both positive and negative tests
+	 * Tests findByRoleName Both positive and negative tests
 	 */
 	@Test
 	@Order(2)
-	public void testFindByRolename() {
-//		// Test on empty table
-//		//Role result = repository.findByName();
-//		
-//		Assertions.assertNull(result);
-//		
-//		
-//		// Test on nonempty table with invalid user name
-//		Role testRole = new Role(1, ERole.ROLE_USER);
-//		repository.save(testRole);
-//		
-//		result = repository.findById();
-//		
-//		Assertions.assertNull(result);
-//		
-//		
-//        // Test on nonempty table with valid user name
-//		result = repository.findByUserName(testRole.getUserName());
-//
-//		Assertions.assertNotNull(result);
-//		Assertions.assertEquals(20, result.getAge());
+	void testFindByRolename() {
+		// Test on empty table
+		Optional<Role> result = repository.findByName(null);
+
+		Assertions.assertNotNull(result, "result is Null");
+		Assertions.assertTrue(!result.isPresent(), "value present");
+
+		// Test on nonempty table with invalid role name
+		Role testRole = new Role(1, ERole.ROLE_USER);
+		repository.save(testRole);
+
+		result = repository.findByName(ERole.ROLE_ADMIN);
+
+		Assertions.assertNotNull(result, "result is Null");
+		Assertions.assertTrue(!result.isPresent(), "value present");
+
+		// Test on nonempty table with valid user name
+		result = repository.findByName(ERole.ROLE_USER);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(ERole.ROLE_USER, result.get().getName());
 	}
 
 }
