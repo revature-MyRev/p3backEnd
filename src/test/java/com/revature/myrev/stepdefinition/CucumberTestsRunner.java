612c6f519e5d2d5665255fff811a9ac7676f09f2
airmanon <anjali.ghosh@revature.net>
2021-11-29 13:46:30 -0800

    Update CucumberTestsRunner.java
    Fixed compiler error

--- src/test/java/com/revature/myrev/stepdefinition/CucumberTestsRunner.java
+++ src/test/java/com/revature/myrev/stepdefinition/CucumberTestsRunner.java
@@ -3,7 +3,7 @@ package com.revature.myrev.stepdefinition;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.CucumberOptions;
-import io.cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = "cucumbertests")
